12a911ffdfc7b6cbc9862b247052793485f8cbc9
Lyle <denmanlp@gmail.com>
2015-12-15 14:01:40 -0600

    Update Makefile; Add ex3.c

---
+++ ex3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 30;
+    int height = 72;
+
+    printf("I am %d years old.\n", age);
+    printf("I am %d inches tall.\n", height);
+    printf("I am a \t horizontal tab.\n");
+
+    return 0;
+}
