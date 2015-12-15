14785726c8d48eae1d18e282b92ad9453b9f49a2
Lyle <denmanlp@gmail.com>
2015-12-15 14:37:04 -0600

    Add ex6.c

--- Makefile
+++ Makefile
@@ -3,6 +3,7 @@ CFLAGS=-Wall -g
 all: 
 	cc ex1.c  -o ex1
 	cc ex3.c  -o ex3
+	cc ex6.c  -o ex6
 
 clean:
 	rm -f ex1 ex3
---
+++ ex6.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int distance = 100;
+    float pi = 3.14;
+    double super_pi = 3.14159;
+
+    char middle_initial = 'P';
+    char first_name[] = "Lyle";
+    char last_name[] = "Denman";
+
+    printf("You are %d miles away from me.\n", distance);
+    printf("Pi = %f.\n", pi);
+    printf("Super Pi = %f.\n", super_pi);
+
+    printf("My middle initial is \'%c\'.\n", middle_initial);
+    printf("My first name is %s.\n", first_name);
+    printf("My last name is %s.\n", last_name);
+
+    printf("My whole name is %s %c %s.\n", first_name, middle_initial, last_name);
+
+
+    return 0;
+}
